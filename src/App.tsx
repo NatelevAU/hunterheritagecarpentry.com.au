da8d787b9559d63b808f98fbd450ea1d3ffdee93
Nathan <nathan.levy.au@gmail.com>
2021-07-29 18:49:19 +1000

    added favicon, logo, homepage coming soon

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
 import React from 'react';
 
 import './App.css';
-import logo from './images/logo.svg';
+import logo from './images/logo.png';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Coming soon</h1>
       </header>
     </div>
   );
