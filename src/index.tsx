1feb9258baea7ddf6e5522c2270d56483a41315b
Nathan <nathan.levy.au@gmail.com>
2021-07-30 19:48:57 +1000

    added logo in more sizes, metadata fix

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,7 @@ import './index.css';
 
 function App({}) {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Switch>
         <Route exact path="/" component={Home} />
         <Route component={PageNotFound} />
