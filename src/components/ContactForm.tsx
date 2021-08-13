1917d8f49f7720fecf953eb4dc77b4cbbd67d142
Nathan <nathan.levy.au@gmail.com>
2021-08-13 13:11:57 +1000

    added more options to contact form, set up formspree submission with axios

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,7 +1,7 @@
 import { Box, Button, TextField, ThemeProvider } from '@material-ui/core';
 import { createTheme } from '@material-ui/core/styles';
 import axios from 'axios';
-import { Form, Formik } from 'formik';
+import { ErrorMessage, Form, Formik } from 'formik';
 import React, { useState } from 'react';
 import * as Yup from 'yup';
 
@@ -24,69 +24,97 @@ const FormSchema = Yup.object().shape({
 });
 
 const FormspreeContact: React.FC<{ formId: string }> = props => {
-  /* Server State Handling */
   const { formId } = props;
-  const url = 'http://formspree.io/'.concat(formId);
-  const isSubmitting = false;
+  const url = 'https://formspree.io/f/'.concat(formId);
   const [serverState, setServerState] = useState<{ ok: boolean; msg: string }>();
+  const [isSubmitting, setSubmitting] = useState<boolean>(false);
   const handleServerResponse = (ok: boolean, msg: string) => {
     setServerState({ ok, msg });
   };
-  const handleOnSubmit = (
-    values: any,
-    actions: { setSubmitting: (arg0: boolean) => void; resetForm: () => void },
-  ) => {
-    axios({
-      method: 'POST',
-      url: url,
-      data: values,
-    })
-      .then((response: any) => {
-        actions.setSubmitting(false);
-        actions.resetForm();
-        handleServerResponse(true, 'Thanks!');
-      })
-      .catch((error: { response: { data: { error: any } } }) => {
-        actions.setSubmitting(false);
-        handleServerResponse(false, error.response.data.error);
-      });
-  };
+
   return (
     <ThemeProvider theme={formTheme}>
       <Box marginLeft="20px" marginRight="20px">
         <Formik
-          initialValues={{ email: '', message: '' }}
-          onSubmit={handleOnSubmit}
-          validationSchema={FormSchema}
+          initialValues={{ name: '', email: '', telephone: '', message: '' }}
+          // validationSchema={FormSchema}
+          onSubmit={(values, { setSubmitting, resetForm }) => {
+            alert(JSON.stringify(values, null, 2));
+            axios({
+              method: 'POST',
+              url: 'url',
+              data: values,
+            })
+              .then((response: any) => {
+                setSubmitting(false);
+                resetForm();
+                handleServerResponse(true, 'Thanks!');
+              })
+              .catch((error: { response: { data: { error: any } } }) => {
+                setSubmitting(false);
+                handleServerResponse(false, error.response.data.error);
+              });
+          }}
         >
-          <Form id="fs-frm" noValidate autoComplete="off">
-            <TextField
-              id="email"
-              label="Email"
-              type="email"
-              variant="filled"
-              fullWidth={true}
-              style={{ marginBottom: '20px' }}
-            />
-            <br></br>
-            {/* <ErrorMessage name="email" className="errorMsg" component="p" /> */}
-            <TextField
-              id="message"
-              label="Message"
-              type="message"
-              variant="filled"
-              multiline
-              minRows={5}
-              fullWidth={true}
-              style={{ marginBottom: '20px' }}
-            />
-            <br></br>
-            {/* <ErrorMessage name="message" className="errorMsg" component="p" /> */}
-            <Button type="submit" disabled={isSubmitting} color="secondary" variant="contained">
-              Send
-            </Button>
-            {serverState && <p className={!serverState.ok ? 'errorMsg' : ''}>{serverState.msg}</p>}
-          </Form>
+          {({ isSubmitting, dirty, submitForm }) => (
+            <Form>
+              <TextField
+                id="name"
+                name="name"
+                label="Name"
+                type="text"
+                variant="filled"
+                fullWidth={true}
+                style={{ marginBottom: '20px' }}
+              />
+              <br></br>
+              <TextField
+                id="email"
+                name="email"
+                label="Email*"
+                type="email"
+                variant="filled"
+                fullWidth={true}
+                style={{ marginBottom: '20px' }}
+              />
+              <br></br>
+              <TextField
+                id="telephone"
+                name="telephone"
+                label="Phone"
+                type="telephone"
+                variant="filled"
+                fullWidth={true}
+                style={{ marginBottom: '20px' }}
+              />
+              <ErrorMessage name="email" className="errorMsg" component="p" />
+              <TextField
+                id="message"
+                name="message"
+                label="Message*"
+                type="message"
+                variant="filled"
+                multiline
+                minRows={5}
+                fullWidth={true}
+                style={{ marginBottom: '20px' }}
+              />
+              <br></br>
+              <ErrorMessage name="message" className="errorMsg" component="p" />
+              <Button
+                type="submit"
+                disabled={isSubmitting}
+                color="secondary"
+                variant="contained"
+                onClick={submitForm}
+              >
+                Send
+              </Button>
+              {serverState && (
+                <p className={!serverState.ok ? 'errorMsg' : ''}>{serverState.msg}</p>
+              )}
+            </Form>
+          )}
         </Formik>
       </Box>
     </ThemeProvider>
