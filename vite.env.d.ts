4c65969938ae5009d6490d26dadb829aa03bd1d2
NatelevAU <nathan.levy.au@gmail.com>
2024-10-31 09:49:56 +1100

    preparing for move to vite

---
+++ vite.env.d.ts
@@ -0,0 +1,10 @@
+/// <reference types="vite/client" />
+
+declare module '*.css';
+declare module '*.scss';
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.png';
+declare module '*.gif';
+declare module '*.svg';
+declare module 'react-image-gallery';
